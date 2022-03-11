689edadd506ab4ea158dab7c23be3dd2e3a8854c
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-11 21:19:44 +0000

    else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,14 +15,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0) {
-		puts ("%d is positive\n");
+	if (n > 0) 
+		{
+		puts("%d is positive\n");
 		}
-	else if (n==0) {
-		puts ("%d is zero\n");
+	else if (n == 0) 
+		{
+		puts("%d is zero\n");
 		}
-	else {
-		puts ("%d is negative\n");
+	else 
+		{
+		puts("%d is negative\n");
 		}
 	return (0);
 }
