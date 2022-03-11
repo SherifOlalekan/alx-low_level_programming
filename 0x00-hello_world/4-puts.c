940bdbfccd2b3467d21c3dad5f47dc9a291500eb
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-11 01:10:19 +0000

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
-*Description: Programming is like building a multilingual puzzle
+* Description: Programming is like building a multilingual puzzle
 *
-*main(void) - Entry point
-*puts: used to display lines in the terminal
-*return: always 0.
+* Main(void) - Entry point
+* puts: used to display lines in the terminal
+* Return: Always 0.
 */
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+
 /**
-*Description: Using printf to display value;
+* Description: Using printf to display value;
 *
-*main - Prints out a string to stdio
+* main - Entry point
 *
-*Return: Always 0(success)
+* Return: Always 0(success)
 *
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
-}
-	
+	return (0);
+}	
