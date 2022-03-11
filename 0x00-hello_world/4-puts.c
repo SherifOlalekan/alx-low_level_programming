06c8fc431c87c789fad69aaa33a7d00eb8b836ed
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-11 00:30:54 +0000

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,14 @@
 *Programming is like building a multilingual puzzle
 *
 *
-*main- entry point
-*void-means there are no input argument
 *puts: used to display lines in the terminal
 *return: return a value which is 0
 */
 int main(void)
+/**
+*main- Entry point, first function to be executed
+*void-no input argument
+*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
