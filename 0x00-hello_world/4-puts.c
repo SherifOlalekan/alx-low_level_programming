db4d72ae866566e9d2a9ea4f51d95273b3356805
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-11 00:12:11 +0000

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 /**
-* main: entry, whic is void 
+*This is a program to use puts function
 *
-* return: return a value which is 0
+* 
+*main: entry point
+*void: means there are no input argument
+*puts: used to display lines in the terminal
+*return: return a value which is 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);     	
+	return (0);
 
 
 }
