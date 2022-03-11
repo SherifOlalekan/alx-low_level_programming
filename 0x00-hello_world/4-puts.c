ead335abe4d9418cb8fc519f1bfece6cb8508c6c
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-11 00:03:53 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main: entry, whic is void 
- *
- * return: return a value which is 0
- */
+* main: entry, whic is void 
+*
+* return: return a value which is 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-         	
+	return (0);     	
 
 
 }
