3e95231b3a1a3f8149bbff77ad3584588128de65
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-11 00:55:56 +0000

    put

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*Description: Using printf to display value;
+*
+*main - Prints out a string to stdio
+*
+*Return: Always 0(success)
+*
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is
+	a piece of art\n");
+	return(0);
+}
+	
