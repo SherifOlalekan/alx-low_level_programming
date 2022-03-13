6684b8ac12c46be2b34a844775c2973eb2872ae8
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 10:52:11 +0000

    num6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @void: return no value it true
+*
+* Return: Always 0 (Success)
+*
+* Program to list all base 10 numberz
+*/
+	int main(void)
+
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n);
+
+	return (0);
+}
