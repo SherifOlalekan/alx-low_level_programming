823faf30aca478fbca01777548517cde454c2955
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 10:14:31 +0000

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @void: return no value whenitis true
+*
+* return: returns 0
+*
+* Program to bring from all numbers in base 10 from 0
+*/
+	int main(void)
+
+{
+	int i = 0;
+		for (i = 0; i < 10; i++)
+	printf("%d", i);
+	return (0);
+}
