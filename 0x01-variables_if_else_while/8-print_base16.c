b210fc93106ba0fec70ef83a1fa67fecda7dd97a
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 11:34:33 +0000

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @void: return no value if true
+*
+* Return: always zero
+*
+*Program to print lower case of base 16
+*/
+	int main(void)
+
+{
+	int ch;
+
+	{
+	for (ch = 0; ch <= 10; ch++)
+	puchar (ch);
+
+	for (ch = '61'; ch <= 67; ch++)
+	putchar (ch);
+	}
+	putchar ('\n');
+
+	return (0);
+}
