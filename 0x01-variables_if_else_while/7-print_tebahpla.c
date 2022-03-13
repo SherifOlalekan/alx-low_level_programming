f08e68adbc5260c3e46f7c139a69bf0087c94086
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 11:10:05 +0000

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @void: return no value if true
+*
+* Return: always 0
+*
+* Program to print alphabet in reverse order
+*/
+
+	int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+		{
+		putchar(ch);
+		}
+		putchar ('\n');
+
+	return (0);
+}
