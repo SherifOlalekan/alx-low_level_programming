fb6f81cf507d3feae5942af57a72afcfff76832f
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 09:23:19 +0000

    alPHA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @void: return no value if true
+*
+* Return: always 0
+*
+* Alphabets in lower and UPPER caSE
+*/
+	int main(void)
+	{
+	int ch;
+	{
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
