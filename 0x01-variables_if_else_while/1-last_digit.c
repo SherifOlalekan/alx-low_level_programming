9b2b77831c23a59acb798afce0e505de55b92c67
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 06:10:42 +0000

    ifelse

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,29 +3,30 @@
 #include <time.h>
 /**
 * main - Entry point
-* @void: returns no value
+* @void: returns no value if true
 *
 * Return: Always 0 (Success)
 *
-* Description: This program display random number when it is run
+* This program display random number when it is run
 */
 int main(void)
 {
 	int n;
 
-        srand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	int ld = n % 10;
 	if (n > 5)
 		{
-		printf("Last digit of is %d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 		}
 	else if (n == 0)
 		{
-		printf("Last digit of is %d and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 		}
 	else if ((n < 6) != 0)
 		{
-		printf("Last digit of is %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 		}
 	return (0);
 }
