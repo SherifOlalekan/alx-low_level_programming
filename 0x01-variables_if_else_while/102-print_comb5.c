0bb7eeb92b9b2c4673c0670e03df780d2ef09069
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 16:51:45 +0000

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,13 +19,13 @@ for (first_nums = 0; first_nums < 100 ; first_nums++)
 for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
 {
 
-putchar (first_nums / 10 + '0'); 
-putchar (first_nums % 10 + '0');
+putchar ((first_nums / 10) + '0'); 
+putchar ((first_nums % 10) + '0');
 
 putchar (' ');
 
-putchar (second_nums / 10 + '0');
-putchar (second_nums % 10 + '0');
+putchar ((second_nums / 10) + '0');
+putchar ((second_nums % 10) + '0');
 
 if (first_nums == 98 && second_nums == 99)
 {
@@ -42,8 +42,4 @@ putchar (' ');
 putchar('\n');
 
 return (0);
-}
-
-return (0);
-
 }
