9968bdbdecf3f07d3f25ca5e44a70f87ea37483a
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 08:40:25 +0000

    ch

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
 *
 * Return: Always 0 (Success)
 */
-	int main (void)
+	int main(void)
 
 	{
 	int ch;
 
-	for(ch = 'A'; ch <= 'Z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	return (0);
 	}
