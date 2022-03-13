8fe49f9e9c48358ecf8391fdaa9f46a30d475898
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 15:29:46 +0000

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - enrty point
+* @void: return no value if true
+*
+* Return: always 0
+*
+* Program to print all combination of 2 digits
+*/
+	int main(void)
+
+{
+	int i;
