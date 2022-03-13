013171bee6643c7821f18452eb9fb167a52fc37a
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 16:41:28 +0000

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,48 @@
 nclude <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* @void: return no value if true
+*
+* Return: Always 0
+*
+* Program to print tab of 2-2 digits
+*/
 int main(void)
 
 {
-int first_nums; /*Decarling statement*/
+int first_nums; 
 int second_nums;
 
-/*for 0-9 with ASCII*/
+
 for (first_nums = 0; first_nums < 100 ; first_nums++)
 {
 for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
 {
 
-putchar (first_nums / 10 + '0'); /*print  first nums, first digit*/
-putchar (first_nums % 10 + '0'); /*print  first nums, second digit*/
+putchar (first_nums / 10 + '0'); 
+putchar (first_nums % 10 + '0');
 
-putchar (' '); /*prirint a space*/
+putchar (' ');
 
-putchar (second_nums / 10 + '0'); /*print  sencond nums, first digit*/
-putchar (second_nums % 10 + '0'); /*print  second nums, second digit*/
+putchar (second_nums / 10 + '0');
+putchar (second_nums % 10 + '0');
 
-/*break program if the condition is true (if is 98 99)*/
 if (first_nums == 98 && second_nums == 99)
 {
 break;
 }
 
-putchar(','); /*print (,)*/
-putchar (' '); /*prirint a space*/
+putchar(',');
+putchar (' ');
 
-} /*end for #2*/
+}
 
-} /*end for #1*/
+}
 
-putchar('\n'); /*new line*/
+putchar('\n');
+
+return (0);
+}
 
 return (0);
 
