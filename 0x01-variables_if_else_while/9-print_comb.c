457d8689e19ccfb87d8688e0aa49b798a1eeeb19
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 11:58:13 +0000

    lok

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @void: return no value if true
+*
+* Return: always 0
+*
+* Program to print all single digits with comma.
+*/
+	int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar(i);
+		{
+		if (i < 9)
+		putchar(", ");
+		}
+	putchar('\n');
+	
+	return(0);
+}
+
