a2ba025355d950154ca8dded5c94086a5d78f56c
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-13 08:36:22 +0000

    ch

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @void: return no value if true
+*
+* Return: Always 0 (Success)
+*/
+	int main (void)
+
+	{
+	int ch;
+
+	for(ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	return (0);
+	}
