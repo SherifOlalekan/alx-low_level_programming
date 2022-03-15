d25881a182cf3a654bf984da5ddb120f4161c474
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-15 23:18:19 +0000

    omo

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _islower - entry point of code
+* @c: return 1 value if true
+*
+* Return: 0 if not true.
+*
+* Program to print lower case
+*/
+
+int _islower(int c)
+
+{
+if (c > 'a' && c < 'z')
+	{
+	return (1);
+	}
+else 
+	{
+	return (0);
+	}
+}
