04d450c0d445f22f4160595167f316258f23d694
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-15 22:49:50 +0000

    alpha10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - entry point of code
+* @void: return no value if true
+*
+* Return: 0 if true.
+*
+* Program to print alphabet 10x
+*/
+        void print_alphabet_x10(void)
+
+{
+        int alphabet = 0;
+        while (alphabet <= 10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+
+		{
+		 _putchar(ch);
+		ch++;
+		}
+	}
+	alphabet++;
+
+	_putchar('\n');
+}
