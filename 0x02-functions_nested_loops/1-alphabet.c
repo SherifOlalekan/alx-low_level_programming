44cd7edf29d58c5066fb736c6efaa3fe1c0084e8
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-15 22:16:49 +0000

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* main - entry point of code
+* @void: return no value if true
+*
+* Return: 0 if true.
+*
+* Program to print _putchar
+*/
+        int main(void)
+
+{
+        int i;
+        char string[26] = "_putchar";
+
+        for (i = a; i <= z; i++)
+        {
+                _putchar(string[i]);
+        }
+
+        _putchar('\n');
+
+        return (0);
+}
