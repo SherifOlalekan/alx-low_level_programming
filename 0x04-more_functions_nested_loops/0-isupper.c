f28049c0b84d3e6aff1611269ed0a49a092da450
Akeem_Sherif_Olalekan <akeemsherif@gmail.com>
2022-03-17 18:17:53 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - Entry point of code to print uppercase
+* @c: return int value
+*
+* Return: 1 if true and 0 if false.
+*
+* Function to check for uppercase character
+*/
+int _isupper(int c);
+{
+	if (c == 'A', c <= 'Z', c++)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is a continuation of ALX low lwvel programming language on C.
+More Functions and Nested Loops
